8ede924ccafcb9f2649921787a6727259095f726
Zhechev <zhechev.mitko@gmail.com>
2025-12-16 12:52:44 +0200

    chore: bootstrap Laravel 12 with Sail (auth, livewire, bootstrap)

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import '@popperjs/core';
+
+import './bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
